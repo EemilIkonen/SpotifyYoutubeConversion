fdc3c8be44098380f94cfdd7b44e0e8328cefe69
EemilIkonen <eemilikonen@gmail.com>
2024-03-08 18:56:52 +0200

    Refactor code and move main.py to src folder

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import re
+import settings
+from googleapiclient.discovery import build
+
+
+spotify_client_id = settings.SPOTIFY_CLIENT_ID
+spotify_client_secret = settings.SPOTIFY_CLIENT_SECRET
+youtube_api_key = settings.YOUTUBE_API_KEY
+
+url = "https://open.spotify.com/track/3n3Ppam7vgaVa1iaRUc9Lp"
+
+spotify_service = SpotifyService(spotify_client_id, spotify_client_secret)
+print(spotify_service.extract_id(url))
+print(spotify_service.get_type(url))
+print(spotify_service.get_track_info(spotify_service.extract_id(url)))
