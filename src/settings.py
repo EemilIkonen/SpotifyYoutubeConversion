68590bb2876f7e67fb8ace0e6facea363f263a60
EemilIkonen <eemilikonen@gmail.com>
2024-03-08 18:56:28 +0200

    Moved settings.py to a subfolder

--- src/settings.py
+++ src/settings.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 # Load .env file
-dotenv_path = join(dirname(__file__), ".env")
+dotenv_path = join(dirname(dirname(__file__)), ".env")
 load_dotenv(dotenv_path)
 
 # Set up Spotify API credentials
