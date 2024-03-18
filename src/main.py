0e8ef9e1a8e148477fbf1ffb2c0c97694f3a76cb
EemilIkonen <eemilikonen@gmail.com>
2024-03-18 19:55:19 +0200

    Changed temporary testing settings in main.py

--- src/main.py
+++ src/main.py
@@ -6,9 +6,12 @@ spotify_client_id = settings.SPOTIFY_CLIENT_ID
 spotify_client_secret = settings.SPOTIFY_CLIENT_SECRET
 youtube_api_key = settings.YOUTUBE_API_KEY
 
-url = "https://open.spotify.com/track/3n3Ppam7vgaVa1iaRUc9Lp"
+url = "https://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6"
 
 spotify_service = SpotifyService(spotify_client_id, spotify_client_secret)
-print(spotify_service.extract_id(url))
-print(spotify_service.get_type(url))
-print(spotify_service.get_track_info(spotify_service.extract_id(url)))
+
+print(
+    spotify_service.get_info(
+        spotify_service.extract_id(url), spotify_service.get_type(url)
+    )["name"]
+)
