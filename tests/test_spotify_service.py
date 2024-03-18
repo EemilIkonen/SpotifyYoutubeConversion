28b069ab20e98bdc95810634d7f749af37844c17
EemilIkonen <eemilikonen@gmail.com>
2024-03-18 19:56:46 +0200

    Add tests for get_info method in SpotifyService

--- tests/test_spotify_service.py
+++ tests/test_spotify_service.py
@@ -65,4 +65,71 @@ class TestSpotifyService(unittest.TestCase):
                 "https://open.spotify.com/playlist/6rqhFgbbKwnb9MLmUQDhG6"
             ),
             "playlist",
+    def test_get_info(self):
+        info = self.service.get_info(
+            "https://open.spotify.com/episode/2alNsrB9p601PFWvt50yZx?si=f59dd04963714d44",
+            "episode",
+        )
+        expected_keys = [
+            "audio_preview_url",
+            "description",
+            "duration_ms",
+            "explicit",
+            "external_urls",
+            "href",
+            "html_description",
+            "id",
+            "images",
+            "is_externally_hosted",
+            "is_playable",
+            "language",
+            "languages",
+            "name",
+            "release_date",
+            "release_date_precision",
+            "show",
+            "type",
+            "uri",
+        ]
+        self.assertCountEqual(
+            info.keys(), expected_keys, "The returned keys are not correct."
+        )
+        self.assertEqual(
+            info["name"], "Guido van Rossum: Python", "The name is not correct."
+        )
+        self.assertEqual(info["id"], "2alNsrB9p601PFWvt50yZx", "The ID is not correct.")
+
+        info2 = self.service.get_info(
+            "https://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6",
+            "track",
+        )
+        expected_keys2 = [
+            "album",
+            "artists",
+            "available_markets",
+            "disc_number",
+            "duration_ms",
+            "explicit",
+            "external_ids",
+            "external_urls",
+            "href",
+            "id",
+            "is_local",
+            "name",
+            "popularity",
+            "preview_url",
+            "track_number",
+            "type",
+            "uri",
+        ]
+        self.assertCountEqual(
+            info2.keys(), expected_keys2, "The returned keys are not correct."
+        )
+        self.assertEqual(
+            info2["name"],
+            "Speak To Me - 2011 Remastered Version",
+            "The name is not correct.",
+        )
+        self.assertEqual(
+            info2["id"], "6rqhFgbbKwnb9MLmUQDhG6", "The ID is not correct."
         )
